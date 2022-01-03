375cbfb0f0804211b8f3ce9fa3d8dc6e0a35ff82
Faisal L <salluthdev@gmail.com>
2022-01-03 20:52:57 +0700

    Penambahan Back To Top - Menu

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+// Back To Top Menu
+var mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 220 || document.documentElement.scrollTop > 220) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// Slider - Swiper JS
 const swiper = new Swiper('.mySwiper', {
   direction: 'horizontal',
   loop: true,
