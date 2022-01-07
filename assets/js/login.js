716d4cd8311ddf603ed8bb9842849ca2bb6e0f76
Faisal L <salluthdev@gmail.com>
2022-01-07 21:02:07 +0700

    Penambahan Media Query CSS - Responsive Member Area HTML

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,20 @@
+// Sidebar Navigation
+$(document).ready(function(){
+  $('.btn-menu').click(function(){
+    $('.side-nav').addClass('active');
+    $('.btn-menu').css("visibility", "hidden");
+    $('.btn-close').css("visibility", "visible");
+    $('.dark').css("display", "block")
+  });
+
+  $('.btn-close').click(function(){
+    $('.side-nav').removeClass('active');
+    $('.btn-menu').css("visibility", "visible")
+    $('.btn-close').css("visibility", "hidden");
+    $('.dark').css("display", "none")
+  });
+});
+
 // Back To Top Menu
 var mybutton = document.getElementById("back-to-top");
 
