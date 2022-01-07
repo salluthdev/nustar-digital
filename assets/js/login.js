2f23c646178fa3815b178bdc74e44a4ac749e12d
Faisal L <salluthdev@gmail.com>
2022-01-07 11:54:22 +0700

    Penambahan Script Login JS

---
+++ assets/js/login.js
@@ -0,0 +1,17 @@
+// Back To Top Menu
+var mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
