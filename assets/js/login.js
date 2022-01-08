2a44c0f81e0dd13d3c1c0ee8b07bad2f76824de2
Faisal L <salluthdev@gmail.com>
2022-01-08 10:17:36 +0700

    Penambahan Menu Accordion - Card Main Container Cara Memulai

--- assets/js/login.js
+++ assets/js/login.js
@@ -32,3 +32,11 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+const accordion = document.getElementsByClassName('content_box');
+
+for (i = 0; i < accordion.length; i++){
+  accordion[i].addEventListener('click', function(){
+    this.classList.toggle('active')
+  })
+}
